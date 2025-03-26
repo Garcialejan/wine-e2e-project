346f2d10f953dd53692ab44da9779f5e4862f953
Alejandro_pc\Alejandro <garcialejan@gmail.com>
2025-03-26 20:17:03 +0100

    Data Ingestion pipeline done

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.wine_e2e_project import logger
+from src.wine_e2e_project.pipeline.data_ingestion_pipeline import DataIngestionPipeline
 
-logger.info("Welcome to our custome logging datascience")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<")
+    obj = DataIngestionPipeline()
+    obj.initiate_data_ingestion
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
